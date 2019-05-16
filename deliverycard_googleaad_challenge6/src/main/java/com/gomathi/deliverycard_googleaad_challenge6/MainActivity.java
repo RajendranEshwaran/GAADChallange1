0c6900e480759bf00944bffa1088cbbce569b7e0
RajendranEshwaran <rajendranmca2010@gmail.com>
2019-05-15 21:12:54 -0400

    google AAD home challange 6 In main activity added the option menus

--- deliverycard_googleaad_challenge6/src/main/java/com/gomathi/deliverycard_googleaad_challenge6/MainActivity.java
+++ deliverycard_googleaad_challenge6/src/main/java/com/gomathi/deliverycard_googleaad_challenge6/MainActivity.java
@@ -1,11 +1,18 @@
 package com.gomathi.deliverycard_googleaad_challenge6;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton imageButton2;
     ImageButton imageButton3;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +31,64 @@ public class MainActivity extends AppCompatActivity {
         imageButton1 = (ImageButton)findViewById(R.id.imageButton);
         imageButton2 = (ImageButton)findViewById(R.id.imageButton2);
         imageButton3 = (ImageButton)findViewById(R.id.imageButton3);
+
+
+    }
+
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.
+     * @param menu The options menu in which you place your items.
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.id_status:
+                Toast.makeText(getApplicationContext(), "status", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id_contact:
+                Toast.makeText(getApplicationContext(), "Contact", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id_favorites:
+                Toast.makeText(getApplicationContext(), "Favorites", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id_order:
+                Toast.makeText(getApplicationContext(), "Orders", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     // Mark:- ImageButton click and goto delivery method activity
+
     public void gotoDeliveryActivity(View view)
     {
 
